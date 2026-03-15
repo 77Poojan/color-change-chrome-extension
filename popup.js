56477d6a7b9f6794ab472f48546f9b0f79e1558c
77Poojan <poojanbade75@gmail.com>
2026-03-15 16:34:11 +0545

    intial commit

---
+++ popup.js
@@ -0,0 +1,10 @@
+document.getElementById("changeColor").addEventListener("click", async () => {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: () => {
+      document.body.style.backgroundColor = "#196283";
+    }
+  });
+});
